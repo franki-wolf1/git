926d9ad37ec084e1fa76ecd8facb6175e7d6f619
Franki Briones <61076196+franki-wolf1@users.noreply.github.com>
2024-04-08 10:19:41 -0500

    Create firts.example.ts

---
+++ firts.example.ts
@@ -0,0 +1,14 @@
+const ejmp_Nv = 'EJEMPLOS DE NIVEL AVANZADO'
+/**Ejemplo de git rebase:
+Supongamos que estás trabajando en una función en una rama llamada mi-funcion 
+y quieres integrar los cambios más recientes de la rama develop en tu rama 
+antes de finalizar tu trabajo:
+
+bash
+
+git checkout mi-funcion
+git rebase develop
+
+Este comando reorganizará la historia de la rama mi-funcion, aplicando los cambios 
+de la rama develop sobre ella.
+ *  */ 
