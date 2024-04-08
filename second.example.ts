9e38bac0bbbb2748fa540f8b9e60c0786405dcfa
Franki Briones <61076196+franki-wolf1@users.noreply.github.com>
2024-04-08 10:19:58 -0500

    Create second.example.ts

---
+++ second.example.ts
@@ -0,0 +1,10 @@
+const title = 'Ejemplo de git cherry-pick:'
+/**Ejemplo de git cherry-pick:
+Imagina que necesitas aplicar un hotfix específico de la rama hotfix a tu rama mi-funcion:
+
+bash
+Copy code
+git checkout mi-funcion 
+git cherry-pick <commit-hash>
+Esto aplicará los cambios del commit identificado por <commit-hash> a tu rama actual.
+ *  */ 
